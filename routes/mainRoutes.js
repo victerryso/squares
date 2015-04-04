608145d39a51dbe5693582a93294df1fb4e13f93
victerry.so <victerry.so@gmail.com>
2015-04-05 02:18:34 +1000

    title and header

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,6 +3,6 @@ Router.route('/', {
   name: 'home',
   action: function () {
     this.render('home');
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
+    SEO.set({ title: 'Squares' });
   }
 });
